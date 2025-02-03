32880b41d430c425487d44319acd44c3c51c2794
im-ushan-ikshana <ikushan23261@gmail.com>
2025-02-03 11:04:02 +0530

    Add App component to integrate WorkoutForm and display title

---
+++ frontend/fitness-coach/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import WorkoutForm from "./components/WorkoutForm";
+
+function App() {
+  return (
+    <div>
+      <h1>AI-Powered Fitness Coach</h1>
+      <WorkoutForm />
+    </div>
+  );
+}
+
+export default App;
